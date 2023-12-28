40fa118eb85c3271feb88c6864543fe45ec69401
DevArtech <100626221+DevArtech@users.noreply.github.com>
2023-12-28 00:28:16 -0600

    Update Lambda Function and requirements.txt

--- server/lambda_function.py
+++ server/lambda_function.py
@@ -1,6 +1,8 @@
 from dbconn import DBConn
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import json
+import awsgi
 
 # App Instance
 app = Flask(__name__)
@@ -16,7 +18,6 @@ def return_home():
 @app.route("/api/v1/submit", methods=["POST"])
 def log_user_data():
     user_data = request.json
-    # dbconn.delete_many("account", "user")
     response = dbconn.insert_doc("account", "user", {"username" : user_data["username"], "password" : user_data["password"]})
     if response.acknowledged:
         logs = []
@@ -25,6 +26,9 @@ def log_user_data():
 
         return jsonify(logs)
     return "Error inserting user"
+
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
