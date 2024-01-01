13aaa9d7defc7c5adbcc70f9d139b8a405af92fa
DevArtech <100626221+DevArtech@users.noreply.github.com>
2023-12-31 22:39:38 -0600

    Update website title

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import './globals.css'
 
 function page() {
+  useEffect(() => {
+    document.title = 'Arctic Designs';
+  }, []);
 
   return (
     <div className="App">
