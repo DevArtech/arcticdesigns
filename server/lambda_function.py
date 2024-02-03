de230bbc626f612761a37e1f56a27b16d79f0824
DevArtech <100626221+DevArtech@users.noreply.github.com>
2024-02-03 15:14:40 -0600

    Fix typo

--- server/lambda_function.py
+++ server/lambda_function.py
@@ -36,7 +36,7 @@ app.secret_key = os.getenv('SECRET_KEY')
 def login():
     return google.authorize_redirect(redirect_uri=url_for('v1/uthorize', _external=True))
 
-@app.route('v1/authorize')
+@app.route('/v1/authorize')
 def authorize():
     code = request.args.get('code', None)
     if code:
