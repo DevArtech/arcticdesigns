6609e44455f705b9fd19b39cb71e075d43425b2a
DevArtech <100626221+DevArtech@users.noreply.github.com>
2024-02-09 11:38:24 -0600

    Update background coloring for input fields

--- app/ProductPage.tsx
+++ app/ProductPage.tsx
@@ -210,7 +210,6 @@ function ProductPage(props: ProductPageProps) {
                     removeArrowOnDeviceType={["tablet", "mobile"]}>
                         { productData.images }
                     </Carousel>
-                    {/* <div className={styles["left-side-padding"]}/> */}
                     <div className={styles["right-product-info"]}>
                         {StarRating(productData.rating)}
                         <p className={styles["product-name"]}>{productData.name}</p>
